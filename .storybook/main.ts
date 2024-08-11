e15ceb10a9f231d252d0cccbd6d776e0abca5eb1
Michael Nau <michaelwnau@gmail.com>
2024-08-11 15:53:07 -0500

    update main.ts for swagger ui

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,11 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import { ExtrasConfig } from '@storybook-extras/preset';
 import path from 'path';
 
-const config: StorybookConfig = {
+const config: StorybookConfig & ExtrasConfig = {
   stories: [
     '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../src/components/**/*.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-onboarding',
@@ -15,7 +14,22 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
     '@storybook/addon-docs',
+    '@storybook-extras/preset',
   ],
+
+  "extras": {
+    "swagger": {
+      "stories": [
+        {
+          // "title": "Swagger UI",
+          // "url": 'https://guardian-portal-storybook.vercel.app/v2/swagger.json',
+          // "path": "../stories/swagger",
+          // "titlePrefix": "Swagger",
+        },
+      ]
+    },
+  },
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
@@ -25,6 +39,8 @@ const config: StorybookConfig = {
     checkOptions: {},
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
+      // Provide the path to your tsconfig.json so that your stories can
+      // display types from outside each individual story.
       tsconfigPath: path.resolve(__dirname, "../tsconfig.json"),
       propFilter: (prop) => {
         if (prop.parent) {
@@ -33,6 +49,18 @@ const config: StorybookConfig = {
         return true
       },
     },
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'next/dist/shared/lib/router-context': 'next/dist/shared/lib/router-context.shared-runtime',
+        'next/dist/shared/lib/head-manager-context': 'next/dist/shared/lib/head-manager-context.shared-runtime',
+        'next/dist/shared/lib/app-router-context': 'next/dist/shared/lib/app-router-context.shared-runtime',
+        'next/dist/shared/lib/hooks-client-context': 'next/dist/shared/lib/hooks-client-context.shared-runtime',
+      };
+    }
+    return config;
   },
 };
 
